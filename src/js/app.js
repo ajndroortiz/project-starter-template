3eb3733dca294e8c18c285748de9505ad56a1870
Alejandro Ortiz <ajndro.ortiz@gmail.com>
2018-05-07 08:21:38 -0400

    processed eslint fixing

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,9 @@ import { person, sayHello, Person } from './lib';
 console.log( person.name );
 console.log( sayHello( 'Alex' ) );
 
-const Alex = new Person("Alex", "Ortiz");
+const Alex = new Person( 'Alex', 'Ortiz' );
 
-console.log(Alex.greeting());
+console.log( Alex.greeting() );
 
 // async function getPosts() {
 //     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
