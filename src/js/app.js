20c144b70a0ebbfb09fc5d1d206954fee7ac5b8b
Alejandro Ortiz <ajndro.ortiz@gmail.com>
2018-07-01 17:28:03 -0400

    fixed styling and js files

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-import { person, sayHello, Person } from './lib';
-=======
 import { person, sayHello, Adult } from './lib';
->>>>>>> updated imagemin task settings
 
-console.log( person.name );
-console.log( sayHello( 'Alex' ) );
+console.log(person.name);
+console.log(sayHello('Alex'));
 
-<<<<<<< HEAD
-const Alex = new Person( 'Alex', 'Ortiz' );
-
-console.log( Alex.greeting() );
-
-// async function getPosts() {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     const data = await response.json();
-//     return data;
-// }
-
-// getPosts().then(posts => console.log(posts));
-=======
 // async function getPosts() {
 //   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 //   const data = await response.json();
@@ -31,4 +14,3 @@ console.log( Alex.greeting() );
 console.log(`${person.name} is ${person.age} years old`);
 const newAdult = new Adult('John', 'Smith');
 newAdult.printName();
->>>>>>> updated imagemin task settings
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,23 +1,4 @@
 export const person = {
-<<<<<<< HEAD
-    name: 'John',
-    location: 'Miami',
-    age: 35
-};
-
-export function sayHello( name ) {
-    return `Hello ${name}`;
-}
-
-export class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    greeting() {
-        return "Hi my name is " + this.firstName + " " + this.lastName;
-    }
-=======
   name: 'John',
   location: 'Miami',
   age: 35,
@@ -32,8 +13,8 @@ export class Adult {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+
   printName() {
     console.log(`${this.firstName} ${this.lastName}`);
   }
->>>>>>> updated imagemin task settings
 }
