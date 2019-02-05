787a60aad7b7656c3f5f677f14ee3be4e9c2614a
Alejandro Ortiz <ajndro.ortiz@gmail.com>
2019-02-05 14:17:27 -0500

    update js settings and added img base styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,17 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single',
-            {'allowTemplateLiterals': true}
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'space-in-parens': [
-            'error',
-            'always'
-        ],
-        'no-console': 'off'
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    'space-in-parens': ['error', 'always'],
+    'no-console': 'off'
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: ['babel-polyfill', './src/js/app.js']
-    },
-    output: {
-        path: path.resolve(__dirname, './build/js'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env', 'stage-0']
-                }
-            }
-        ]
-    }
+  mode: 'development',
+  entry: {
+    app: ['babel-polyfill', './src/js/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './build/js'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
 };
